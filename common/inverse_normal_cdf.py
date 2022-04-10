d4ea7de2542aae9f64386eb9ba0db0b30580611f
Maximalfocus <21332201+zhubaiyuan@users.noreply.github.com>
2022-04-10 17:09:08 +0800

    flipping a coin

---
+++ common/inverse_normal_cdf.py
@@ -0,0 +1,21 @@
+from common.normal_cdf import normal_cdf
+
+
+def inverse_normal_cdf(p: float,
+                       mu: float = 0,
+                       sigma: float = 1,
+                       tolerance: float = 0.00001) -> float:
+    """Find approximate inverse using binary search"""
+    # if not standard, compute standard and rescale
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+    low_z = -10.0                      # normal_cdf(-10) is (very close to) 0
+    hi_z = 10.0                        # normal_cdf(10)  is (very close to) 1
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z + hi_z) / 2     # Consider the midpoint
+        mid_p = normal_cdf(mid_z)      # and the cdf's value there
+        if mid_p < p:
+            low_z = mid_z              # Midpoint too low, search above it
+        else:
+            hi_z = mid_z               # Midpoint too high, search below it
+    return mid_z
---
+++ common/normal_cdf.py
@@ -0,0 +1,5 @@
+import math
+
+
+def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
